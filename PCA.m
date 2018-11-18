8de2685eccabec51d41e53f3d64d8087e9aac97d
rupturous <44347316+rupturous@users.noreply.github.com>
2018-11-18 15:37:16 +0000

    Update PCA.m
    changed variable names, added/deleted input/output variables
    added the NN classification section, currently not identifying the correct image

--- PCA.m
+++ PCA.m
@@ -1,6 +1,6 @@
 %Pattern Recognition Coursework 1
 %eigenvectors with non zero eigenvalues, M eigenvectors
-function [V_sorted, D_sorted] = PCA(tr_data, M, nR, te_data, nC)
+function [V_sorted, D_sorted, minDistIdx] = PCA(tr_data, tr_label, M, nR, te_data, te_label, nC)
 
 %mean image
 im_sum = zeros(size(tr_data,1),1);
@@ -50,12 +50,12 @@ im_c = tr_data(:,nR);
 im = reshape(im_c, [56,46]);
 im = imrotate(im.',270); 
 
-weights = [1,M];
+w_r = [1,M];
 sum = zeros(size(im_c, 1), 1);
 sub = im_c - avg_t;
 for i=1:M
-    weights(i) = dot(sub, V_sorted(:,i));
-    sum = sum + (weights(i)*V_sorted(:,i));
+    w_r(i) = dot(sub, V_sorted(:,i));
+    sum = sum + (w_r(i)*V_sorted(:,i));
 end
 
 im_r1 = avg_t + sum;
@@ -67,8 +67,36 @@ figure(2);
 subplot(1,2,1),image(im);title('Original Image')
 subplot(1,2,2),image(im_r);title(['Reconstructed Image, M = ', num2str(M)])
 
+%face recognition: Nearest Neighbor Classification
+im_t = te_data(:,nC);
+w_t = [1,M];
+for i=1:M
+    w_t(i) = dot((im_t - avg_t),V_sorted(:,i));
+end
 
+euDist = [1, size(tr_data, 1)];
+for i=1:size(tr_data, 2) % i = image index in training data
+    w_n = [];
+    for j=1:M %j = eigenvector index in sorted matrix/number of weights
+        w_n(j) = dot(tr_data(:,i),V_sorted(:,j));
+    end %completed weights vector for one training image
+    %create vector of dists between test image weights and each training image weights
+    euDist(i) = norm(w_t - w_n);             
+end
+[~, minDistIdx] = min(euDist);
+%minDistIdx refers to the index of the 'closest' image match from the training data
 
+%display test image used and the closest match from the training data
+im_t = reshape(im_t, [56,46]);
+im_t = imrotate(im_t.',270); 
+im_m = reshape(tr_data(:,minDistIdx), [56, 46]);
+im_m = imrotate(im_m.',270);
+
+figure(3);
+subplot(1,2,1), image(im_t); 
+title(['Testing Image, Identity #', num2str(te_label(1,nC))])
+subplot(1,2,2), image(im_m); 
+title(['Closest Match, Identity #', num2str(tr_label(1, minDistIdx))])
 
 end
 
