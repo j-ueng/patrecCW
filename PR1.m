e96e9cace77c7142595130f6db3a65d765d18506
rupturous <44347316+rupturous@users.noreply.github.com>
2018-11-18 15:34:57 +0000

    Update PR1.m
    Changed input/output variables for PCA

--- PR1.m
+++ PR1.m
@@ -16,7 +16,8 @@ for i = 1:max(l) %cover all the photos
         nte = nte +1;
         
 end
-[V1, D1] = PCA(tr_data, 100, 208); 
+[V1, D1, minIdx] = PCA(tr_data, tr_label, 50, 208, te_data, te_label, 104); 
+
 %[V2, D2] = PCA2(tr_data, 50, 208);
 
 
