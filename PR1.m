9e395cbe9ea97fd18ec8b482fadc5a4fc1f3d7a5
jyepark <jyepark95@gmail.com>
2018-11-20 17:46:55 +0000

    PR1: runs altRecog and NNRecog agianst many M values

--- PR1.m
+++ PR1.m
@@ -1,4 +1,7 @@
 %Pattern Recognition Coursework 1
+close all
+clear
+clc
 
 %data partitioning
 load('face(1).mat');
@@ -22,8 +25,15 @@ end
 [V2, D2, ~] = PCA2(tr_data, 50);
 
 M = [10 25 50 100 200];
-% faces = zeros(size(tr_data,1), 5);
-faces = faceReconst(xbar, X(:,208), V2, M);
-%[euDist, minIdx] = NNRecog(tr_data, tr_label, 100, te_data, te_label, 104, V2, xbar);
-[altAcc, altFail] = altRecog(te_data, te_label, tr_data, tr_label, 5);
-[NNAcc, NNFail] = NNRecog(tr_data, tr_label, 50, te_data, te_label, V2, xbar);
+[faces, recon_e] = faceReconst(xbar, X(:,208), V2, M);
+
+[altAcc1, altFail1] = altRecog(te_data, te_label, tr_data, tr_label, 1);
+[altAcc2, altFail2] = altRecog(te_data, te_label, tr_data, tr_label, 2);
+[altAcc3, altFail3] = altRecog(te_data, te_label, tr_data, tr_label, 4);
+[altAcc4, altFail4] = altRecog(te_data, te_label, tr_data, tr_label, 7);
+
+[NNAcc1, NNFail1] = NNRecog(tr_data, tr_label, 25, te_data, te_label, V2, xbar);
+[NNAcc2, NNFail2] = NNRecog(tr_data, tr_label, 50, te_data, te_label, V2, xbar);
+[NNAcc3, NNFail3] = NNRecog(tr_data, tr_label, 100, te_data, te_label, V2, xbar);
+[NNAcc4, NNFail4] = NNRecog(tr_data, tr_label, 200, te_data, te_label, V2, xbar);
+[NNAcc5, NNFail5] = NNRecog(tr_data, tr_label, 400, te_data, te_label, V2, xbar);
