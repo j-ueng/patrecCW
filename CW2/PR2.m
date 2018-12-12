54dc6107ad73bf52f3d53fc3a86fa6832a20f740
jillueng <44347316+jillueng@users.noreply.github.com>
2018-12-12 18:18:41 +0000

    returns accurate scores over whole query set for k = 1, 5, 10

--- CW2/PR2.m
+++ CW2/PR2.m
@@ -9,20 +9,42 @@
 %use Euclidean distance
 
 success = zeros(1, 3);
+query_used = 1400;
 i = 1;
 
-for n = 1:length(gallery_idx)
-    gallery(n, :) = features(n, :);
-end
- 
+% for n = 1:length(gallery_idx)
+%     gallery(n, :) = features(gallery_idx(n), :);
+% end
+
 for k = [1, 5, 10]
-    for q = 1:300
+    for q = 1:query_used
         query = features(query_idx(q),:);
-        ranklist = knnsearch(gallery, query, 'K', k, 'Distance', 'euclidean');
         query_label = labels(query_idx(q));
-    
+        m = 1;
+        discarded = 0;
+        for n = 1:length(gallery_idx)
+            gallery_label = labels(gallery_idx(n));
+            
+            %exclude image if same label + same camID
+            if gallery_label == query_label 
+                if camId(gallery_idx(n)) ~= camId(query_idx(q))
+                    gallery(m, :) = features(gallery_idx(n), :);
+                    store_gallery(m) = gallery_idx(n);
+                    m = m + 1;
+                else
+                    discarded = discarded + 1;
+                end
+            elseif gallery_label ~= query_label
+                gallery(m, :) = features(gallery_idx(n), :);
+                store_gallery(m) = gallery_idx(n);
+                m = m + 1;
+            end
+        end
+        fprintf('q = %d, discarded = %d\n', q, discarded);
+        ranklist = knnsearch(gallery, query, 'K', k, 'Distance', 'euclidean');
+        
         for n = 1:length(ranklist)
-            gallery_label = labels(ranklist(n));
+            gallery_label = labels(store_gallery(ranklist(n)));
             if gallery_label == query_label 
                 success(1,i) = success(1,i) + 1;
                 break
@@ -31,4 +53,4 @@ for k = [1, 5, 10]
     end
     i = i + 1;
 end
-% scores = success/length(query_idx);
+scores = success/query_used;
