b22181ab06b1af4b4876931a51e82f0c261e24b9
jillueng <44347316+jillueng@users.noreply.github.com>
2018-12-12 23:44:52 +0000

    displaying the images correctly -need to add borders

--- CW2/knn_test.m
+++ CW2/knn_test.m
@@ -1,41 +1,38 @@
-q = 60;
+q = 1400;
 k = 5;
 query = features(query_idx(q),:);
-m = 1;
 query_label = labels(query_idx(q));
-discarded = 0;
+m = 1;
+
 for n = 1:length(gallery_idx)
     gallery_label = labels(gallery_idx(n));
-      
+            
     %exclude image if same label + same camID
-    if gallery_label == query_label 
-         if camId(gallery_idx(n)) ~= camId(query_idx(q))
-                gallery(m, :) = features(n, :);
+        if gallery_label == query_label 
+            if camId(gallery_idx(n)) ~= camId(query_idx(q))
+                gallery(m, :) = features(gallery_idx(n), :);
+                store_gallery(m) = gallery_idx(n);
                 m = m + 1;
-         elseif camId(gallery_idx(n)) == camId(query_idx(q))
-             discarded = discarded + 1;
-         end
-    elseif gallery_label ~= query_label
-         gallery(m, :) = features(n, :);
-         m = m + 1;
-    end
+            end
+        elseif gallery_label ~= query_label
+            gallery(m, :) = features(gallery_idx(n), :);
+            store_gallery(m) = gallery_idx(n);
+            m = m + 1;
+        end
 end
-m = m - 1;
+
 ranklist = knnsearch(gallery, query, 'K', k, 'Distance', 'euclidean');
-
-success = 0;
+        
 for n = 1:length(ranklist)
-    show_gallery_label(n) = labels(ranklist(n));
+    gallery_label = labels(store_gallery(ranklist(n)));
+        if gallery_label == query_label 
+            success = 1;
+            break
+        end
 end
-
 for n = 1:length(ranklist)
-    gallery_label = labels(ranklist(n));
-    if gallery_label == query_label 
-        success = 1;
-        break
-    end
+    show_label(n) = labels(store_gallery(ranklist(n)));
 end
-
 
 %display query image / images in ranklist
 figure(1);
@@ -46,7 +43,7 @@ subplot(1,k + 1,1), imshow(im_q);
 
 %display images in ranklist
 for i = 1:k
-    imtitle = char(filelist(ranklist(i)));
+    imtitle = char(filelist(store_gallery(ranklist(i))));
     im_g = imread(imtitle);
     im_g = imresize(im_g, [323, 155]);
     subplot(1, k + 1, i + 1), imshow(im_g);
